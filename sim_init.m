5ee4b03a84a4a4d8710912a2b3f73b544946e0ae
Carlos Alberto M Massera Filho <massera@icmc.usp.br>
2016-09-16 10:15:37 -0300

    Retune gains related to cross-track error over speed
    Issue: #21 Retune the controller to reduce cost w.r.t cross-track error

--- sim_init.m
+++ sim_init.m
@@ -107,13 +107,13 @@ display('Setting controller parameters')
 controller_parameters = struct();
 
 % Controller sampling time
-controller_parameters.Ts = 1e-1; % 10Hz
+controller_parameters.Ts = 5e-2; % 20Hz
 
 % Controller weights
 controller_parameters.tau = 2;
 controller_parameters.C = [1 0 0 0; 0 0 0 0];
 controller_parameters.D = [0; 1];
-controller_parameters.W = @(vx)(diag([max(vx,5) / 8, 10 * pi / 180])^-2); 
+controller_parameters.W = @(vx)(diag([max(vx, 5) / 5, 10 * pi / 180])^-2); 
 
 display(['    Completed, ' num2str(toc) ' seconds elapsed']);
 
@@ -124,7 +124,7 @@ observer_parameters = struct();
 
 % Observer sampling time
 observer_parameters.Ts = controller_parameters.Ts;
-observer_parameters.subsampling = 1;
+observer_parameters.subsampling = 2; % 10 Hz
 
 % Observer parameters
 observer_parameters.rho = 1e-3;
